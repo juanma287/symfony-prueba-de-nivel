bcbbb6cbe02641c353401a65aa1cd273462f51d0
Juan Manuel Lazzarini <juanma_287@hotmail.com>
2021-04-14 10:37:16 -0700

    Precarga de datos update

---
+++ symfony/src/DataFixtures/AppFixtures.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Empresa;
+use App\Entity\Sector;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+ 
+        $sector = new Sector();
+        $sector->setNombre('Finanzas');
+        $manager->persist($sector);
+
+        $sector2 = new Sector();
+        $sector2->setNombre('Automotor');
+        $manager->persist($sector2);
+      
+        $manager->flush();
+
+        $emmpresa = new Empresa();
+        $emmpresa->setNombre('JP Morgan');
+        $emmpresa->setEmail('juanma@gmail.com');
+        $emmpresa->setTelefono('3498 479936');
+        $emmpresa->setSector($sector);
+        $manager->persist($emmpresa);
+      
+        $manager->flush();
+
+
+
+
+       
+    }
+}
